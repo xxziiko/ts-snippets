d6489d9c6a04ec721e4b8dad6487d415c90b805f
xxziiko <xxziiko@gmail.com>
2024-05-06 00:46:49 +0900

    fix: 배열의 인덱스를 활용하여 풀이

--- programmers/피보나치_수.js
+++ programmers/피보나치_수.js
@@ -1,12 +1,10 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12945
 
 function solution(n) {
-  const fibonacciNumber = (num) => {
-    if (num <= 1) return num;
-    return fibonacciNumber(num - 1) + fibonacciNumber(num - 2);
-  };
+  let fibonacci = [0, 1];
+  for (let i = 2; i <= n; i++)
+    // 오버플로우가 일어나지 않게 모든 단계에서 나누기
+    fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % 1234567;
 
-  return fibonacciNumber(n);
+  return fibonacci[n];
 }
-
-console.log(solution(4));
