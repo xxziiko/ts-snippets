c4e979b56dd5cf88deb5e02d1dce007dc97eff3f
xxziiko <xxziiko@gmail.com>
2024-05-06 23:36:50 +0900

    fix: reduce와 재귀함수를 이용한 풀이

--- programmers/N개의_최소공배수.js
+++ programmers/N개의_최소공배수.js
@@ -1,22 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12953
 
-function solution(arr) {
-  let n = 0;
-  let m = 0;
-  let GCD = 0;
-  let result = 0;
-
-  while (arr.length) {
-    if (n === 0) n = arr.shift();
-    else n = result;
-    m = arr.shift();
-
-    for (let i = 1; i <= m; i++) {
-      if (n % i === 0 && m % i === 0) GCD = i;
-    }
-
-    result = (n * m) / GCD;
-  }
-
-  return result;
+function nlcm(num) {
+  return num.reduce((a, b) => (a * b) / gcd(a, b));
 }
+
+function gcd(a, b) {
+  return a % b ? gcd(b, a % b) : b;
+}
+
+console.log(nlcm([2, 6, 8, 14]));
