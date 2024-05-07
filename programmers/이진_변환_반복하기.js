d525457dd69b3aee3967ee8b35ed9a12ca1ee754
xxziiko <xxziiko@gmail.com>
2024-05-07 16:11:53 +0900

    fix: 피드백 반영

--- programmers/이진_변환_반복하기.js
+++ programmers/이진_변환_반복하기.js
@@ -1,18 +1,21 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/70129
 
 function solution(s) {
-	const answer = [0, 0];
+	let count = 0;
+	let zeroCount = 0;
 	let number = s;
 	let stringLength = s.length;
 
 	while (number > 1) {
-		for (const i of number) i === "0" && answer[1]++;
+		for (const i of number) {
+			if (i === "0") zeroCount += 1;
+		}
 
 		number = number.replaceAll("0", "");
 		stringLength = number.length;
 		number = stringLength.toString(2);
-		answer[0] += 1;
+		count += 1;
 	}
 
-	return answer;
+	return [count, zeroCount];
 }
