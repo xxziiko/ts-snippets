3696ae9df2d9e60001131b5718e68f7b23c9221e
xxziiko <xxziiko@gmail.com>
2024-05-07 21:27:04 +0900

    fix: 중복제거

--- programmers/N개의_최소공배수.js
+++ programmers/N개의_최소공배수.js
@@ -1,11 +1,10 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12953
 
 function nlcm(num) {
-  return num.reduce((a, b) => (a * b) / gcd(a, b));
+	return num.reduce((a, b) => (a * b) / gcd(a, b));
 }
 
 function gcd(a, b) {
-  return a % b ? gcd(b, a % b) : b;
+	const value = a % b;
+	return value ? gcd(b, value) : b;
 }
-
-console.log(nlcm([2, 6, 8, 14]));
