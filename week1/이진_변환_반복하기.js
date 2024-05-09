06be45df4344011be206f2cf3bd88e160fabc368
xxziiko <xxziiko@gmail.com>
2024-05-09 15:12:46 +0900

    etc: 폴더 이름 변경

--- week1/이진_변환_반복하기.js
+++ week1/이진_변환_반복하기.js
@@ -3,9 +3,9 @@
 function solution(s) {
 	let count = 0;
 	let zeroCount = 0;
-	let number = s;
+	// let number = s;
 
-	while (number > 1) {
+	while (s > 1) {
 		for (const i of number) {
 			if (i === "0") zeroCount += 1;
 		}
