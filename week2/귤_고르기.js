00f8983817833452dce31f479ee87abbdf57fe12
xxziiko <xxziiko@gmail.com>
2024-05-12 16:58:06 +0900

    fix: 피드백 반영

--- week2/귤_고르기.js
+++ week2/귤_고르기.js
@@ -3,13 +3,15 @@
 function solution(k, tangerine) {
 	const map = new Map();
 	let sum = 0;
+	let count = 0;
 
-	for (const item of tangerine) map.set(item, (map.get(item) || 0) + 1);
+	for (const item of tangerine) map.set(item, (map.get(item) ?? 0) + 1);
 
 	const sortedList = [...map.values()].sort((a, b) => b - a);
 
 	for (let i = 0; i <= sortedList.length; i++) {
-		if (sum >= k) return sortedList.slice(0, i).length;
+		if (sum >= k) return count;
 		sum += sortedList[i];
+		count += 1;
 	}
 }
