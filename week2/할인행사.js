dcedeef24db07b8f312954f534b31744314bcfee
xxziiko <xxziiko@gmail.com>
2024-05-13 18:11:14 +0900

    feat: 할인행사.js

---
+++ week2/할인행사.js
@@ -0,0 +1,25 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/131127
+
+const getLength = (array) => array.length;
+
+function solution(want, numbers, discount) {
+	let count = 0;
+
+	for (let i = 0; i < discount.length; i++) {
+		const map = new Map();
+
+		for (const item of discount.slice(i, 10 + i)) {
+			if (want.includes(item)) map.set(item, (map.get(item) ?? 0) + 1);
+
+			if (
+				getLength([...map]) === getLength(want) &&
+				[...map.keys()].every(
+					(product) => map.get(product) === numbers[want.indexOf(product)],
+				)
+			)
+				count += 1;
+		}
+	}
+
+	return count;
+}
