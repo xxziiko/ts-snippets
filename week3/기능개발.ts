c7b25ce9c1ee188269f0658488b16166f1a66d90
xxziiko <xxziiko@gmail.com>
2024-05-21 15:04:13 +0900

    refac: 피드백 반영

--- week3/기능개발.ts
+++ week3/기능개발.ts
@@ -1,14 +1,16 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42586
+import _ from "lodash";
 
 function solution(progresses: number[], speeds: number[]) {
-	const dates = [];
 	const answer = [];
-
-	for (const [i, per] of progresses.entries())
-		dates.push(Math.ceil((100 - per) / speeds[i]));
-
+	const dates = _.zipWith(
+		progresses,
+		speeds,
+		(progress: number, speed: number) => Math.ceil((100 - progress) / speed),
+	);
 	let prevDate = dates[0];
 	let count = 0;
+
 	for (const date of dates) {
 		if (date <= prevDate) count++;
 		else {
