d6f93b822e192dfe331b2fad92e673f77b3d7c67
xxziiko <xxziiko@gmail.com>
2024-05-21 21:25:09 +0900

    etc: 피드백

--- week1/올바른_괄호.js
+++ week1/올바른_괄호.js
@@ -1,15 +1,28 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12909
 
 function solution(s) {
-	const stack = [];
+	// 맞는 코드 > 틀린 코드
+	// 맞고 맞다는걸 확신시키는 코드 > 맞고 아리까리한 코드
+	// 맞아보이는 코드, 틀려보이는 코이드
+	// for (const i of s) {
+	// 	if (stack[0] === ")") return false; // stack.length === 1 ")"
+	// 	if (stack.at(-1) === "(" && i === ")") stack.pop(); // "("
+	// 	else stack.push(i); // 빈 stack에다가 ")" push
+	// }
 
-	if (s.length % 2 === 1) return false;
-
+	// O(N)
+	let openCount = 0;
 	for (const i of s) {
-		if (stack[0] === ")") return false;
-		if (stack.at(-1) === "(" && i === ")") stack.pop();
-		else stack.push(i);
+		if (i === "(") {
+			openCount++;
+		} else {
+			if (openCount > 0) {
+				openCount--;
+			} else {
+				return false;
+			}
+		}
 	}
 
-	return stack.length === 0;
+	return openCount === 0;
 }
