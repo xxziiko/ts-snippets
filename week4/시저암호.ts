299934d9633ddf29b07496beba5f2570865df11d
xxziiko <xxziiko@gmail.com>
2024-05-27 19:18:55 +0900

    refac: 숫자 변수화

--- week4/시저암호.ts
+++ week4/시저암호.ts
@@ -1,20 +1,23 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12926
 
 function solution(s: string, n: number) {
+	const ASCII_A = 65;
+	const ASCII_Z = 90;
+	const ASCII_a = 97;
+	const ASCII_z = 122;
 	let result = "";
 
 	for (const char of s) {
 		const charCode = char.charCodeAt(0);
 		let ASCII = charCode + n;
 
-		if (charCode >= 65 && charCode <= 90 && ASCII > 90)
-			ASCII = 65 + (ASCII - 91);
-		if (charCode >= 97 && charCode <= 122 && ASCII > 122)
-			ASCII = 97 + (ASCII - 123);
+		if (charCode >= ASCII_A && charCode <= ASCII_Z && ASCII > ASCII_Z)
+			ASCII = ASCII_A + (ASCII - (ASCII_Z + 1));
+		if (charCode >= ASCII_a && charCode <= ASCII_z && ASCII > ASCII_z)
+			ASCII = ASCII_a + (ASCII - (ASCII_z + 1));
 
 		if (char !== " ") result += String.fromCharCode(ASCII);
 		else result += char;
 	}
-
 	return result;
 }
