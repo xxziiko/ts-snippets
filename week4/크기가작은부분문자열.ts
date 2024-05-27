8c3c0b016404d771e00820b3e11fe95fd2588011
xxziiko <xxziiko@gmail.com>
2024-05-27 20:19:52 +0900

    refac: 피드백 반영

--- week4/크기가작은부분문자열.ts
+++ week4/크기가작은부분문자열.ts
@@ -3,32 +3,14 @@
 function solution(t: string, p: string) {
 	const size = p.length;
 	let count = 0;
-	const gen = slidingWindow(t, size);
+	let window = t.substring(0, size);
+	const pNumber = Number(p);
 
-	for (const str of gen) {
-		const window = str.join("");
-		if (+window <= +p) count++;
+	for (let i = size; i <= t.length; i++) {
+		if (Number(window) <= pNumber) count++;
+		window += t[i];
+		window = window.substring(1);
 	}
 
 	return count;
-}
-
-function* slidingWindow(xs: Iterable<string>, size: number) {
-	const iterator = xs[Symbol.iterator]();
-	const window = [];
-	let result = iterator.next();
-
-	while (!result.done && window.length < size) {
-		window.push(result.value);
-		result = iterator.next();
-	}
-
-	if (window.length === size) yield window;
-
-	while (!result.done) {
-		window.shift();
-		window.push(result.value);
-		yield window;
-		result = iterator.next();
-	}
 }
