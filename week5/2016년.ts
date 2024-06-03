f5587a83053af6008f193d5adae46077e81c0df5
xxziiko <xxziiko@gmail.com>
2024-06-03 19:14:04 +0900

    refac: date 제거 및 리팩토링

--- week5/2016년.ts
+++ week5/2016년.ts
@@ -1,6 +1,13 @@
-function solution(a: number, b: number) {
-	const WEEKDAY = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-	const findIndex = new Date(2016, a - 1, b).getDay();
+// https://school.programmers.co.kr/learn/courses/30/lessons/12901
 
-	return WEEKDAY[findIndex];
+function solution(a: number, b: number) {
+	const WEEKDAY = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
+	const MONTH = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	let sum = 0;
+
+	for (let i = 0; i < a - 1; i++) sum += MONTH[i];
+	sum += b;
+
+	const index = (sum % 7 || WEEKDAY.length) - 1;
+	return WEEKDAY[index];
 }
