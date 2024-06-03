d2f98e5db639bc76540277fbd559a00f51584915
xxziiko <xxziiko@gmail.com>
2024-06-03 22:14:24 +0900

    refac: magic number로 변경

--- week5/2016년.ts
+++ week5/2016년.ts
@@ -1,13 +1,14 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12901
 
 function solution(a: number, b: number) {
-	const WEEKDAY = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
+	const ONEWEEK = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
 	const MONTH = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	const weekLen = ONEWEEK.length;
 	let sum = 0;
 
 	for (let i = 0; i < a - 1; i++) sum += MONTH[i];
 	sum += b;
 
-	const index = (sum % 7 || WEEKDAY.length) - 1;
-	return WEEKDAY[index];
+	const index = (sum % weekLen || weekLen) - 1;
+	return ONEWEEK[index];
 }
