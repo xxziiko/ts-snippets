cb90851e33ac8350345002e2f9b349d5f651c276
xxziiko <xxziiko@gmail.com>
2024-06-03 13:54:33 +0900

    refac: 피드백 반영

--- week5/덧칠하기.ts
+++ week5/덧칠하기.ts
@@ -1,18 +1,14 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/161989
 
-const fillArray = (num: number) => {
-	return new Array(num).fill(false);
-};
-
 function solution(n: number, m: number, section: number[]) {
-	const area = fillArray(n);
+	const area = new Array(n).fill(false);
 	let count = 0;
 
 	for (const number of section) area[number - 1] = true;
 
 	for (const [i, target] of area.entries()) {
 		if (target) {
-			area.splice(i, m, ...fillArray(m));
+			area.splice(i, m, ...new Array(m).fill(false));
 			count++;
 		}
 	}
