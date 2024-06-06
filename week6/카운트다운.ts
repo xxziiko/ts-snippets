411711cd3fd66fd7ee3178838c7229111b7a94e1
xxziiko <xxziiko@gmail.com>
2024-06-06 12:12:50 +0900

    feat: 카운트다운

---
+++ week6/카운트다운.ts
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181899
+
+function solution(start_num: number, end_num: number) {
+	const result = [];
+	for (let i = start_num; i >= end_num; i--) result.push(i);
+	return result;
+}
