eae5f754eb8f9e2dfa45e8467df3a78f9ecbb55f
xxziiko <xxziiko@gmail.com>
2024-06-07 15:10:16 +0900

    refac: 피드백 반영

--- week4/시저암호.ts
+++ week4/시저암호.ts
@@ -4,6 +4,7 @@ function solution(s: string, n: number) {
 	const ASCII_A = "A".charCodeAt(0);
 	const ASCII_Z = "Z".charCodeAt(0);
 	const ASCII_a = "a".charCodeAt(0);
+	const ALPHABET_COUNT = 26;
 	let result = "";
 
 	for (const char of s) {
@@ -12,9 +13,10 @@ function solution(s: string, n: number) {
 		const index = isUpper ? charCode % ASCII_A : charCode % ASCII_a;
 		const baseCharCode = isUpper ? ASCII_A : ASCII_a;
 
-		if (char !== " ")
-			result += String.fromCharCode(baseCharCode + ((index + n) % 26));
-		else result += char;
+		result +=
+			char !== " "
+				? String.fromCharCode(baseCharCode + ((index + n) % ALPHABET_COUNT))
+				: char;
 	}
 
 	return result;
