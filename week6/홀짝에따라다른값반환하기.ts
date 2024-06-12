32fb56c4d63347411394f2b3c425018d01aeecfd
xxziiko <xxziiko@gmail.com>
2024-06-12 21:02:10 +0900

    refac: 피드백 반영

--- week6/홀짝에따라다른값반환하기.ts
+++ week6/홀짝에따라다른값반환하기.ts
@@ -3,8 +3,11 @@
 function solution(n: number) {
 	let result = 0;
 
-	if (n % 2 === 1) for (let i = 1; i <= n; i += 2) result += i;
-	if (n % 2 === 0) for (let i = 2; i <= n; i += 2) result += i * i;
+	const f = n % 2 === 0 ? (x: number) => x * x : (x: number) => x;
+
+	for (let i = n % 2; i <= n; i += 2) {
+		result += f(i);
+	}
 
 	return result;
 }
