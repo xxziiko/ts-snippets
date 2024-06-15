58dc4d0c8af4871f1f07734454935b5cfb38fcca
xxziiko <xxziiko@gmail.com>
2024-06-15 13:45:34 +0900

    feat: 로그인성공

---
+++ week7/로그인성공.ts
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120883
+
+function solution(id_pw: string[], db: string[][]) {
+	const [inputId, inputPw] = id_pw;
+
+	for (const [id, pw] of db) {
+		if (inputId === id && inputPw === pw) return "login";
+		if (inputId === id && inputPw !== pw) return "wrong pw";
+	}
+
+	return "fail";
+}
