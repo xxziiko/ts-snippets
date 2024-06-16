11d89e7504cc68d84574c184bf24ada6dce832f5
xxziiko <xxziiko@gmail.com>
2024-06-16 22:31:23 +0900

    refac: Math.max 함수 적용

--- week7/두수의연산값비교하기.ts
+++ week7/두수의연산값비교하기.ts
@@ -1,8 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181938
 
 function solution(a: number, b: number) {
-	const newNumber = Number(String(a) + b);
+	const newNumber = Number(String(a) + String(b));
 	const product = 2 * a * b;
 
-	return newNumber >= product ? newNumber : product;
+	return Math.max(newNumber, product);
 }
