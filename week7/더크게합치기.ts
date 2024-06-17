0be5b4d08ff63492d009a92ac9486696965676ee
xxziiko <xxziiko@gmail.com>
2024-06-17 22:20:45 +0900

    refac: 중복제거(?)

--- week7/더크게합치기.ts
+++ week7/더크게합치기.ts
@@ -1,5 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181939
 
+const numberToString = (num1: number, num2: number) => {
+	return Number(String(num1) + String(num2));
+};
+
 function solution(a: number, b: number) {
-	return Math.max(Number(String(a) + String(b)), Number(String(b) + String(a)));
+	return Math.max(numberToString(a, b), numberToString(b, a));
 }
