15d8bb7798a41a0b27fd55404ddd8097db203af2
xxziiko <xxziiko@gmail.com>
2024-06-18 21:10:41 +0900

    refac: 피드백 반영

--- week7/치킨쿠폰.ts
+++ week7/치킨쿠폰.ts
@@ -1,14 +1,10 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120884
 
-const calculateMaxServiceChicken = (coupon: number): number => {
+function solution(chicken: number): number {
 	const serviceCount = 10;
-	if (coupon < serviceCount) return 0;
+	if (chicken < serviceCount) return 0;
 
-	const freeChicken = Math.floor(coupon / 10);
-	const restCoupon = (coupon % serviceCount) + freeChicken;
-	return Math.floor(coupon / 10) + calculateMaxServiceChicken(restCoupon);
-};
-
-function solution(chicken: number) {
-	return calculateMaxServiceChicken(chicken);
+	const freeChicken = Math.floor(chicken / 10);
+	const restCoupon = (chicken % serviceCount) + freeChicken;
+	return Math.floor(chicken / 10) + solution(restCoupon);
 }
