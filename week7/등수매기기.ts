00dd8de4e1b555525dc71631d8d0fd1d4f071ccd
xxziiko <xxziiko@gmail.com>
2024-06-18 15:31:30 +0900

    refac: 효율개선(?)

--- week7/등수매기기.ts
+++ week7/등수매기기.ts
@@ -1,8 +1,22 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120882
 
 function solution(score: number[][]) {
-	const average = score.map(([english, math]) => (english + math) / 2);
-	const sortedAverage = average.slice().sort((a, b) => b - a);
+	const averages = score
+		.map(([english, math], index) => [(english + math) / 2, index])
+		.sort((a, b) => b[0] - a[0]);
 
-	return average.map((number) => sortedAverage.indexOf(number) + 1);
+	let prevAverage = -1;
+	let rank = 1;
+	const result = averages
+		.map(([average, prevIndex], index) => {
+			if (prevAverage === average) return [rank, prevIndex];
+			rank = index + 1;
+			prevAverage = average;
+
+			return [rank, prevIndex];
+		})
+		.sort((a, b) => a[1] - b[1])
+		.map(([rank, prevIndex]) => rank);
+
+	return result;
 }
