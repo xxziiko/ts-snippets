f1bcd6bd9f7e56a8e47edf47ce68a122d6ceb0d5
xxziiko <xxziiko@gmail.com>
2024-06-18 22:09:54 +0900

    refac: 코드개선

--- week7/더크게합치기.ts
+++ week7/더크게합치기.ts
@@ -1,9 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181939
 
-const numberToString = (num1: number, num2: number) => {
-	return Number(String(num1) + String(num2));
-};
-
 function solution(a: number, b: number) {
-	return Math.max(numberToString(a, b), numberToString(b, a));
+	const ab = Number(String(a) + String(b));
+	const ba = Number(String(b) + String(a));
+
+	return ab === ba ? ab : Math.max(ab, ba);
 }
