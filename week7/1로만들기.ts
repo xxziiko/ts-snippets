f7562007d9697f88216218eb383cfdd5bbb3c813
xxziiko <xxziiko@gmail.com>
2024-06-18 21:09:11 +0900

    refac: reduce 제거, 함수명 변경

--- week7/1로만들기.ts
+++ week7/1로만들기.ts
@@ -1,12 +1,17 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181880
 
-const calculateSteps = (num: number): number => {
+const countStepsToOne = (num: number): number => {
 	if (num === 1) return 0;
-	return num % 2 === 1 ? calculateSteps(num - 1) : calculateSteps(num / 2) + 1;
+
+	return num % 2 === 1
+		? countStepsToOne(num - 1)
+		: countStepsToOne(num / 2) + 1;
 };
 
 function solution(num_list: number[]) {
-	return num_list
-		.map((num) => calculateSteps(num))
-		.reduce((acc, curr) => acc + curr, 0);
+	let sum = 0;
+
+	for (const num of num_list) sum += countStepsToOne(num);
+
+	return sum;
 }
