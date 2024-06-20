9702ad4485e9bb0e2e3c1b74138f092a22fe5bfc
xxziiko <xxziiko@gmail.com>
2024-06-20 16:23:53 +0900

    refac: 재귀함수 리팩토링

--- week7/소수만들기.ts
+++ week7/소수만들기.ts
@@ -6,28 +6,38 @@ const isPrime = (num: number) => {
 	return true;
 };
 
-const sumNumbers: number[] = [];
-const combinations: number[] = [];
-const getSumCombination = (numbers: number[], count: number) => {
-	const nextCount = count - 1;
+const getSumCombinations = (numbers: number[], count: number) => {
+	const sumNumbers: number[] = [];
 
-	if (count === 0) {
-		const sum = combinations.reduce((acc, num) => acc + num, 0);
-		sumNumbers.push(sum);
-		return;
+	function go(
+		numbers: number[],
+		count: number,
+		combinations: number[],
+		sumNumbers: number[],
+	) {
+		if (count === 0) {
+			let sum = 0;
+
+			for (const combination of combinations) sum += combination;
+			sumNumbers.push(sum);
+
+			return sumNumbers;
+		}
+
+		for (const [i, number] of numbers.entries()) {
+			combinations.push(number);
+			go(numbers.slice(i + 1), count - 1, combinations, sumNumbers);
+			combinations.pop(); // 백트래킹
+		}
 	}
 
-	for (const [i, number] of numbers.entries()) {
-		combinations.push(number);
-		getSumCombination(numbers.slice(i + 1), nextCount);
-		combinations.pop(); // 백트래킹
-	}
+	go(numbers, count, [], sumNumbers);
+	return sumNumbers;
 };
 
 function solution(nums: number[]) {
 	let count = 0;
-
-	getSumCombination(nums, 3);
+	const sumNumbers: number[] = getSumCombinations(nums, 3);
 
 	for (const number of sumNumbers) {
 		if (isPrime(number)) count++;
