a3ed77b5af40d32c7a886ffdc54184a17a4d6cf8
xxziiko <xxziiko@gmail.com>
2024-06-21 10:47:22 +0900

    refac: 리팩토링

--- week7/더크게합치기.ts
+++ week7/더크게합치기.ts
@@ -1,8 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181939
 
 function solution(a: number, b: number) {
-	const ab = Number(String(a) + String(b));
-	const ba = Number(String(b) + String(a));
+	const ab = String(a) + String(b);
+	const ba = String(a) + String(b);
+	const isSameString = ab.split("").every((char) => ab.charAt(0) === char);
 
-	return ab === ba ? ab : Math.max(ab, ba);
+	return isSameString ? Number(ab) : Math.max(Number(ab), Number(ba));
 }
