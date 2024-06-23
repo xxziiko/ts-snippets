d9f507d9044e9a37ea4e85c245b885dc72bea6d1
xxziiko <xxziiko@gmail.com>
2024-06-23 22:16:36 +0900

    refac: 중복계산제거

--- week7/더크게합치기.ts
+++ week7/더크게합치기.ts
@@ -1,11 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181939
 
 function solution(a: number, b: number) {
-	const numberAsString = (num1: number, num2: number) =>
-		String(num1) + String(num2);
+	const stringA = String(a);
+	const stringB = String(b);
 
-	const ab = numberAsString(a, b);
-	const ba = numberAsString(b, a);
-
-	return ab > ba ? Number(ab) : Number(ba);
+	return Math.max(Number(stringA + stringB), Number(stringB + stringA));
 }
