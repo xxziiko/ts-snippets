7e8559a54d0c786fced0d903eb0ea005fae9fbec
xxziiko <xxziiko@gmail.com>
2024-06-25 14:30:04 +0900

    refac: 중복제거

--- week8/문자열이몇번등장하는지세기.ts
+++ week8/문자열이몇번등장하는지세기.ts
@@ -1,9 +1,10 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181871
 
-function solution(myString: string, pat: string): number {
+function solution(myString: string, pat: string): number | string {
 	if (!myString.length) return 0;
+	const nextString = myString.substring(1);
 
-	return myString.substring(0, pat.length) === pat
-		? 1 + solution(myString.substring(1), pat)
-		: solution(myString.substring(1), pat);
+	return myString.startsWith(pat)
+		? 1 + solution(nextString, pat)
+		: solution(nextString, pat);
 }
