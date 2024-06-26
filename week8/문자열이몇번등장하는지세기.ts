0e0325f7567ef3cc3cded60c305ab077c1c34d9f
xxziiko <xxziiko@gmail.com>
2024-06-26 21:17:16 +0900

    refac: 피드백 반영

--- week8/문자열이몇번등장하는지세기.ts
+++ week8/문자열이몇번등장하는지세기.ts
@@ -1,10 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181871
 
-function solution(myString: string, pat: string): number | string {
-	if (!myString.length) return 0;
-	const nextString = myString.substring(1);
+function solution(myString: string, pat: string): number {
+	if (myString.length < pat.length) return 0;
 
-	return myString.startsWith(pat)
-		? 1 + solution(nextString, pat)
-		: solution(nextString, pat);
+	return (
+		(myString.startsWith(pat) ? 1 : 0) + solution(myString.substring(1), pat)
+	);
 }
