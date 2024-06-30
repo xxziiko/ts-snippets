f9366948b2b1c12f06f6b9e9961587d33bc7ae51
xxziiko <xxziiko@gmail.com>
2024-06-30 20:35:50 +0900

    refac: 메서드 수정

--- week9/전국대회선발고사.ts
+++ week9/전국대회선발고사.ts
@@ -3,8 +3,8 @@
 function solution(rank: number[], attendance: boolean[]) {
 	const [a, b, c] = rank
 		.flatMap((num, index) => (attendance[index] ? [[index, num]] : []))
-		.sort((a, b) => a[1] - b[1])
-		.slice(0, 3);
+		.sort(([indexA, rankA], [indexB, rankB]) => rankA - rankB)
+		.flatMap(([index, rank], i) => (i < 3 ? [index] : []));
 
-	return a[0] * 10000 + b[0] * 100 + c[0];
+	return a * 10000 + b * 100 + c;
 }
