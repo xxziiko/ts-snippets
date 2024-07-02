965cd70056f9b4d7cc586a02153ac4833b8a81e2
xxziiko <xxziiko@gmail.com>
2024-07-02 15:15:44 +0900

    refac: 클로저 적용

--- week9/문자열밀기.ts
+++ week9/문자열밀기.ts
@@ -1,19 +1,20 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120921
 
-const rotateAndCompareString = (
-	currentString: string,
-	targetString: string,
-	count: number,
-): number => {
-	const nextString = currentString.at(-1) + currentString.slice(0, -1);
-
-	if (currentString === targetString || count === 0) return 0;
-
-	return 1 + rotateAndCompareString(nextString, targetString, count - 1);
-};
-
 function solution(A: string, B: string) {
-	const rotateCount = rotateAndCompareString(A, B, A.length);
+	function rotateAndCompareString(
+		currentString: string,
+		count: number,
+	): number {
+		const nextString = currentString.at(-1) + currentString.slice(0, -1);
 
-	return rotateCount === A.length ? -1 : rotateCount;
+		if (currentString === B) return 0;
+		if (count === 0) return -1;
+
+		const result = rotateAndCompareString(nextString, count - 1);
+		return result === -1 ? -1 : 1 + result;
+	}
+
+	const rotateCount = rotateAndCompareString(A, A.length);
+
+	return rotateCount;
 }
