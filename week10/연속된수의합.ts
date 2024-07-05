fbeaab24f5e009915bf46ab445a6a06442504857
xxziiko <xxziiko@gmail.com>
2024-07-05 17:08:42 +0900

    feat: 연속된 수의 합

---
+++ week10/연속된수의합.ts
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120923
+
+function solution(num: number, total: number) {
+	const mid = Math.floor(total / num);
+	const start = num % 2 === 1 ? Math.floor(num / 2) : Math.floor(num / 2) - 1;
+	let element = mid - start;
+	const result = [];
+
+	for (let i = 0; i < num; i++) {
+		result.push(element);
+		element++;
+	}
+	return result;
+}
