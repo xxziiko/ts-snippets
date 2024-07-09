5bf95c165ebc68cc8776d50b663b5df82841bbd2
xxziiko <xxziiko@gmail.com>
2024-07-10 01:32:21 +0900

    refac: filter와 map으로 변경

--- week10/배열만들기5.ts
+++ week10/배열만들기5.ts
@@ -1,8 +1,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181912
 
 function solution(intStrs: string[], k: number, s: number, l: number) {
-	return intStrs.flatMap((string) => {
-		const num = Number(string.slice(s, s + l));
-		return num > k ? [num] : [];
-	});
+	return intStrs
+		.map((string) => Number(string.slice(s, s + l)))
+		.filter((num) => num > k);
 }
