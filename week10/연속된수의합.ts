a22601218c65d247ecafb14f7ad2871a39bff015
xxziiko <xxziiko@gmail.com>
2024-07-10 01:32:42 +0900

    refac: 중복계산 개선

--- week10/연속된수의합.ts
+++ week10/연속된수의합.ts
@@ -2,7 +2,9 @@
 
 function solution(num: number, total: number) {
 	const mid = Math.floor(total / num);
-	const start = num % 2 === 1 ? Math.floor(num / 2) : Math.floor(num / 2) - 1;
+	const dividedNumber = Math.floor(num / 2);
+	Math.floor(num / 2);
+	const start = num % 2 === 1 ? dividedNumber : dividedNumber - 1;
 	let element = mid - start;
 	const result = [];
 
