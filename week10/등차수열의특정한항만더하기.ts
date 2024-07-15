fb7fc7ca6bdcbf35f087036c9ad458dd2aee93d6
xxziiko <xxziiko@gmail.com>
2024-07-15 16:21:40 +0900

    refac: 컨벤션 수정

--- week10/등차수열의특정한항만더하기.ts
+++ week10/등차수열의특정한항만더하기.ts
@@ -1,11 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181931
 
-function solution(a: number, b: number, included: boolean[]) {
+function solution(a: number, b: number, includeds: boolean[]) {
 	let answer = 0;
 	let next = a;
 
-	for (const boolean of included) {
-		if (boolean) answer += next;
+	for (const included of includeds) {
+		if (included) answer += next;
 		next += b;
 	}
 
