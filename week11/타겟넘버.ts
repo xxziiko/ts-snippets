2fdaed5e1c59ad3a57cc35fac6d65faffa70535d
xxziiko <xxziiko@gmail.com>
2024-07-16 14:56:32 +0900

    refac: index 매개변수 제거

--- week11/타겟넘버.ts
+++ week11/타겟넘버.ts
@@ -1,15 +1,11 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
-function solution(
-	numbers: number[],
-	target: number,
-	index = 0,
-	sum = 0,
-): number {
-	if (index === numbers.length) return target === sum ? 1 : 0;
+function solution(numbers: number[], target: number, sum = 0): number {
+	const num = numbers[0];
+	if (!numbers.length) return target === sum ? 1 : 0;
 
-	const add = solution(numbers, target, index + 1, sum + numbers[index]);
-	const subtract = solution(numbers, target, index + 1, sum - numbers[index]);
+	const add = solution(numbers.slice(1), target, num + sum);
+	const subtract = solution(numbers.slice(1), target, num - sum);
 
 	return add + subtract;
 }
