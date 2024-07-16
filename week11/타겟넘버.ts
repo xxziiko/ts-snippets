a0d0c43d11d7481958698f312475040b70c4656e
xxziiko <xxziiko@gmail.com>
2024-07-16 22:10:10 +0900

    refac: num 위치 변경

--- week11/타겟넘버.ts
+++ week11/타겟넘버.ts
@@ -1,8 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
 function solution(numbers: number[], target: number, sum = 0): number {
-	const num = numbers[0];
 	if (!numbers.length) return target === sum ? 1 : 0;
+	const num = numbers[0];
 
 	const add = solution(numbers.slice(1), target, num + sum);
 	const subtract = solution(numbers.slice(1), target, num - sum);
