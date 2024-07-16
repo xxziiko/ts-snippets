80a4a2da8fde086fa8038eb135b33a24d4a366c7
xxziiko <xxziiko@gmail.com>
2024-07-16 14:15:19 +0900

    refac: 피드백 반영

--- week11/타겟넘버.ts
+++ week11/타겟넘버.ts
@@ -1,14 +1,15 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
-function solution(numbers: number[], target: number) {
-	function dfs(index = 0, sum = 0): number {
-		if (index === numbers.length) return target === sum ? 1 : 0;
+function solution(
+	numbers: number[],
+	target: number,
+	index = 0,
+	sum = 0,
+): number {
+	if (index === numbers.length) return target === sum ? 1 : 0;
 
-		const add = dfs(index + 1, sum + numbers[index]);
-		const subtract = dfs(index + 1, sum - numbers[index]);
+	const add = solution(numbers, target, index + 1, sum + numbers[index]);
+	const subtract = solution(numbers, target, index + 1, sum - numbers[index]);
 
-		return add + subtract;
-	}
-
-	return dfs();
+	return add + subtract;
 }
