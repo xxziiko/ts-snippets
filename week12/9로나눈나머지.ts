0f28aed1caea18696d0aa6367984484d453c0a09
xxziiko <xxziiko@gmail.com>
2024-07-23 23:39:12 +0900

    refac: lodash sum 적용

--- week12/9로나눈나머지.ts
+++ week12/9로나눈나머지.ts
@@ -1,10 +1,10 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181914
+import _ from "lodash";
 
 function solution(number: string) {
 	const nine = 9;
-	let sum = 0;
 
-	for (const num of number.split("")) sum += Number(num);
+	const sum = _.sum(number.split("").map((el) => Number(el)));
 
 	return sum % nine;
 }
