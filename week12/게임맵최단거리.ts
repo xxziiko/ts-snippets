fdc7bec63cb617d59bd4374ad6b90c2bcf947c35
xxziiko <xxziiko@gmail.com>
2024-07-25 00:03:37 +0900

    refac: 피드백 반영

--- week12/게임맵최단거리.ts
+++ week12/게임맵최단거리.ts
@@ -5,41 +5,38 @@ function solution(maps: number[][]) {
 	const colLen = maps[0].length;
 	const rowGoal = rowLen - 1;
 	const colGoal = colLen - 1;
-	const direction = [
+	const direction: [dx: number, dy: number][] = [
 		[1, 0],
 		[-1, 0],
 		[0, 1],
 		[0, -1],
 	];
+	// as const
 
 	const bfs = () => {
 		const queue = [[0, 0, 1]];
 		maps[0][0] = 0;
 
-		while (!queue.length) {
-			const [row, col, distance] = queue[0];
+		while (true) {
+			const first = queue.shift();
+			if (!first) break;
 
-			// 도착지
+			const [row, col, distance] = first;
+
+			if (maps[row][col] === 1) continue;
+
 			if (row === rowGoal && col === colGoal) return distance;
 
 			for (const [r, c] of direction) {
 				const newRow = row + r;
 				const newCol = col + c;
-
-				if (
-					newRow >= 0 &&
-					newRow < rowLen &&
-					newCol >= 0 &&
-					newCol < colLen &&
-					maps[newRow][newCol] === 1
-				) {
+				if (newRow >= 0 && newRow < rowLen && newCol >= 0 && newCol < colLen) {
 					queue.push([newRow, newCol, distance + 1]);
-					maps[newRow][newCol] = 0;
 				}
 			}
 		}
 
-		return -1;
+		throw new Error("이 코드는 실행되면 안됨");
 	};
 
 	return bfs();
