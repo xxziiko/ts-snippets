8a359e4c7ef4af4d4ace1dfd78987a588ca00d48
xxziiko <xxziiko@gmail.com>
2024-08-02 15:28:15 +0900

    refac: result 제거

--- week13/간단한식계산하기.ts
+++ week13/간단한식계산하기.ts
@@ -1,14 +1,13 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181865
 
 function solution(binomial: string) {
-	let result = 0;
 	const [a, symbol, b] = binomial.split(" ");
 	const num1 = Number(a);
 	const num2 = Number(b);
 
-	if (symbol === "+") result = num1 + num2;
-	else if (symbol === "-") result = num1 - num2;
-	else result = num1 * num2;
-
-	return result;
+	return symbol === "+"
+		? num1 + num2
+		: symbol === "-"
+			? num1 - num2
+			: num1 * num2;
 }
